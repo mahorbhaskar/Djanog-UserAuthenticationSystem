5d8fb098246c9ee314f722d3b83b2505434cccbb
Bhaskar Mahor <bhaskarmahor1@gmail.com>
2021-12-31 21:50:32 +0530

    Merge branch 'main' of https://github.com/mahorbhaskar/Djanog-UserAuthenticationSystem

--- assignment2/settings.py
+++ assignment2/settings.py
@@ -133,8 +133,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sheenu4new@gmail.com'
-EMAIL_HOST_PASSWORD = 'Shivam@123'
+EMAIL_HOST_USER = 'Your Email'
+EMAIL_HOST_PASSWORD = 'Your Password'
 
 
 # Default primary key field type
